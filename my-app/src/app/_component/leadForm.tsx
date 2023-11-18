5f1c6d07afba75f6642cf36809af36a013e6f62d
gui-sa <guime.sa9@gmail.com>
2023-11-18 12:03:28 -0300

    Adiciona POST action e TOAST

--- my-app/src/app/_component/leadForm.tsx
+++ my-app/src/app/_component/leadForm.tsx
@@ -1,9 +1,9 @@
-'use client'
+'use client';
 
-import { useForm } from "react-hook-form"
-import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod"
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -12,9 +12,11 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { supabase } from "@/lib/supabase";
+import { useToast } from "@/components/ui/use-toast";
+import { ToastAction } from "@/components/ui/toast"
 
 export const phoneRegex = new RegExp(
   /^\d{2}\d{9}$/
@@ -22,6 +24,10 @@ export const phoneRegex = new RegExp(
 
 
 export function LeadForm() {
+  
+  const { toast } = useToast()
+
+
 
   const formSchema = z.object({
     name: z.string().min(1,{message:"Por favor, digite seu nome"}),
@@ -34,13 +40,35 @@ export function LeadForm() {
     resolver: zodResolver(formSchema),
   })
   
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values)
+  const cleanForm = function(){
     form.reset({
       name:"",
       email:"",
       tel: undefined
     })
+  }
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+      (async()=>{
+        const data = await supabase
+        .from('lead')
+        .insert({ name: values.name, email: values.email, tel:values.tel });
+
+        if(data.status === 201){
+          cleanForm();
+          toast({
+            title: "Enviado com Sucesso",
+            variant: "sucessfull",
+            description: "Obrigado pela inscrição!",
+          })
+        }else if(data.status === 409){
+            toast({
+              title: "Inscrição não concluida",
+              variant: "destructive",
+              description: "Esse e-mail e/ou telefone já existem!",
+            })
+        }
+      })();
   }
   return(
       <Form {...form}>
---
+++ my-app/src/lib/supabase.ts
@@ -0,0 +1,4 @@
+
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient('https://fswowuepzbxskpezxypo.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZzd293dWVwemJ4c2twZXp4eXBvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDAxMzI3NjQsImV4cCI6MjAxNTcwODc2NH0.u0WvN2i8dQn6eImG_tCyPYlbaNN1hP55-uOpuG_R304')
